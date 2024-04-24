9d72e296455fdf95c18dabca2e7312b91928498d
Nadji Henni Saiah <saiahh.nadji@gmail.com>
2024-04-24 02:51:18 +0200

    Normalement dico complet

---
+++ frontend/website/javascript/langDict.js
@@ -0,0 +1,205 @@
+// langDict.js
+var langDict = {
+    'en': {
+        //navbar
+        'home': 'Home',
+        'language': 'Language',
+        'account': 'Account',
+        'logout': 'Logout',
+        'register': 'Register',
+    
+        //https://localhost:4242/firstPage + https://localhost:4242/loginContainer
+        'login': 'Login',
+        'register': 'Register',
+        'password': 'Password',
+    
+        // https://localhost:4242/register
+        'username': 'Username',
+        'password': 'Password',
+        'full name': 'Full Name',
+        'birth date': 'Birth Date',
+        'bio': 'Bio',
+        
+        // https://localhost:4242/homepage
+        'single player': 'Single Player',
+        'local multiplayer': 'Local Multiplayer',
+        'distant multiplayer': 'Distant Multiplayer',
+        'tournament': 'Tournament',
+        'statistics': 'Statistics',
+        'settings': 'Settings',
+        
+        // https://localhost:4242/playerStats
+        'player statistics': 'Player Statistics',
+        'last games results': 'Last Games Results',
+        'player vs player': 'Player vs Player',
+        'score': 'Score',
+        'duration': 'Duration',
+        'created': 'Created',
+        'player details': 'Player Details',
+        'games played': 'Games Played',
+        'total wins': 'Total Wins',
+        'total losses': 'Total Losses',
+        'total score': 'Total Score',
+        'chart of winrate over time': 'Chart of Winrate Over Time',
+    
+        // https://localhost:4242/settings
+        'player 1': 'Player 1',
+        'player 2': 'Player 2',
+        'ball speed': 'Ball Speed',
+        'paddle speed': 'Paddle Speed',
+        'winning score': 'Winning Score',
+        'save': 'Save',
+        'cancel': 'Cancel',
+    
+        //https://localhost:4242/multiplayer
+        'users connected to the room': 'Users Connected to the Room',
+        'room name': 'Room Name',
+        'invite a player': 'Invite a Player',
+        'enter username': 'Enter Username',
+        'rooms': 'Rooms',
+    
+        //https://localhost:4242/tournament
+        'tournament setup': 'Tournament Setup',
+        'match tree': 'Match Tree',
+        'tournament_matches_display': 'Tournament matches will be displayed here once setup is complete.',
+        'enter username to': 'Enter Username to',
+    },
+    
+    'fr': {
+        //navbar
+        'home': 'Accueil',
+        'language': 'Langue',
+        'account': 'Compte',
+        'logout': 'Déconnexion',
+        'register': 'Inscription', //a double virer si conflit
+
+        //https://localhost:4242/firstPage + https://localhost:4242/loginContainer
+        'login': 'Connexion',
+        'register': 'Inscription',
+        'password': 'Mot de passe', //a double virer si conflit
+
+
+
+        // https://localhost:4242/register
+        'register': 'Inscription', //a double virer si conflit
+        'username': 'Nom d’utilisateur',
+        'password': 'Mot de passe',
+        'full name': 'Nom complet',
+        'birth date': 'Date de naissance',
+        'bio': 'Biographie',
+    
+        // https://localhost:4242/homepage
+        'single player': 'Joueur seul',
+        'local multiplayer': 'Multijoueur local',
+        'distant multiplayer': 'Multijoueur à distance',
+        'tournament': 'Tournoi',
+        'statistics': 'Statistiques',
+        'settings': 'Paramètres',
+    
+        // https://localhost:4242/playerStats
+        'player statistics': 'Statistiques du joueur',
+        'last games results': 'Résultats des derniers jeux',
+        'player vs player': 'Joueur contre Joueur',
+        'score': 'Score',
+        'duration': 'Durée',
+        'created': 'Créé le',
+        'player details': 'Détails du joueur',
+        'games played': 'Parties jouées',
+        'total wins': 'Total des victoires',
+        'total losses': 'Total des défaites',
+        'total score': 'Score total',
+        'chart of winrate over time': 'Graphique des taux de victoire dans le temps',
+
+
+        // https://localhost:4242/settings
+        'player 1': 'Joueur 1',
+        'player 2': 'Joueur 2',
+        'ball speed': 'Vitesse de la balle',
+        'paddle speed': 'Vitesse de la raquette',
+        'winning score': 'Score gagnant',
+        'save': 'Sauvegarder',
+        'cancel': 'Annuler',
+
+        //https://localhost:4242/multiplayer
+        
+        'users connected to the room': 'Utilisateurs connectés',
+        'room name': 'Nom de la salle',
+        'invite a player': 'inviter un joueur',
+        'enter username': 'Entrer utilisateur',
+        'rooms': 'Salles',
+
+        //https://localhost:4242/tournament
+        'tournament setup': 'Configuration du Tournoi',
+        'match tree': 'Prochains matchs',
+        'tournament_matches_display': 'Les matchs du tournoi seront affichés ici une fois la configuration terminée.',
+        'enter username to' : 'Entrez le nom utilisateur a',
+
+
+    },
+    
+    'es': {
+        //navbar
+        'home': 'Inicio',
+        'language': 'Idioma',
+        'account': 'Cuenta',
+        'logout': 'Cerrar sesión',
+        'register': 'Registrarse',
+    
+        //https://localhost:4242/firstPage + https://localhost:4242/loginContainer
+        'login': 'Iniciar sesión',
+        'register': 'Registrarse',
+        'password': 'Contraseña',
+    
+        // https://localhost:4242/register
+        'username': 'Nombre de usuario',
+        'password': 'Contraseña',
+        'full name': 'Nombre completo',
+        'birth date': 'Fecha de nacimiento',
+        'bio': 'Biografía',
+        
+        // https://localhost:4242/homepage
+        'single player': 'Un jugador',
+        'local multiplayer': 'Multijugador local',
+        'distant multiplayer': 'Multijugador a distancia',
+        'tournament': 'Torneo',
+        'statistics': 'Estadísticas',
+        'settings': 'Configuraciones',
+        
+        // https://localhost:4242/playerStats
+        'player statistics': 'Estadísticas del jugador',
+        'last games results': 'Resultados de los últimos juegos',
+        'player vs player': 'Jugador contra Jugador',
+        'score': 'Puntuación',
+        'duration': 'Duración',
+        'created': 'Creado',
+        'player details': 'Detalles del jugador',
+        'games played': 'Juegos jugados',
+        'total wins': 'Total de victorias',
+        'total losses': 'Total de derrotas',
+        'total score': 'Puntuación total',
+        'chart of winrate over time': 'Gráfico de tasa de victorias a lo largo del tiempo',
+    
+        // https://localhost:4242/settings
+        'player 1': 'Jugador 1',
+        'player 2': 'Jugador 2',
+        'ball speed': 'Velocidad del balón',
+        'paddle speed': 'Velocidad de la paleta',
+        'winning score': 'Puntuación para ganar',
+        'save': 'Guardar',
+        'cancel': 'Cancelar',
+    
+        //https://localhost:4242/multiplayer
+        'users connected to the room': 'Usuarios conectados a la sala',
+        'room name': 'Nombre de la sala',
+        'invite a player': 'Invitar a un jugador',
+        'enter username': 'Ingresar nombre de usuario',
+        'rooms': 'Salas',
+    
+        //https://localhost:4242/tournament
+        'tournament setup': 'Configuración del torneo',
+        'match tree': 'Árbol de partidos',
+        'tournament_matches_display': 'Los partidos del torneo se mostrarán aquí una vez completada la configuración.',
+        'enter username to': 'Ingresar nombre de usuario para',
+    }
+    
+};
